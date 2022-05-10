220c526d23fdfd255c3c9845ccdf6da31d79d34b
gerfabris <ggerfabris@gmail.com>
2022-05-09 21:10:17 -0300

    Cambios en header, carousel, compatibilidad de animaciones

--- js/carouselSwipper.js
+++ js/carouselSwipper.js
@@ -13,4 +13,8 @@ var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
     },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
