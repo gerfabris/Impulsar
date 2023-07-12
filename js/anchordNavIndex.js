24f42ceb460c30216fe87baad6fd16c2a4013174
gerfabris <ggerfabris@gmail.com>
2023-07-12 20:59:39 -0300

    Fixed errors

--- js/anchordNavIndex.js
+++ js/anchordNavIndex.js
@@ -1,8 +1,7 @@
 const inicio = document.getElementById("anchordNavInicio");
-const arriba = inicio.addEventListener("click", (e)=>{
+inicio.addEventListener("click", (e)=>{
     window.scrollTo({
         top: 0,
         behavior: "smooth"
     });
 })
-arriba();
