0edf7a44d2afbcf89b4e57ff4e7e9b0ddc0caf9b
gerfabris <ggerfabris@gmail.com>
2023-07-25 13:08:55 -0300

    Fixed errors

--- js/navbarBlue.js
+++ js/navbarBlue.js
@@ -3,3 +3,4 @@ const particles = document.getElementsByClassName("particles-js-canvas-el");
 let canva = particles.item(0)
 
 const blue = canva.classList.add("particles__blue");
+submenu.classList.add("submenu-blue");
